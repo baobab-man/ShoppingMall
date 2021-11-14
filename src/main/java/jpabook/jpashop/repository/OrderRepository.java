ce6f3ce9b99767dca8c39db12332881562825dfc
romeo <ii.kim@fitpet.co.kr>
2021-11-14 18:29:17 +0900

    주문 목록 검색, 취소

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -12,6 +12,7 @@ import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -33,21 +34,21 @@ public class OrderRepository {
   /**
    *  JPA Criteria
    */
-  public List<Order> findAllByCriteria(OrderSearchRepository orderSearchRepository) {
+  public List<Order> findAllByCriteria(OrderSearch orderSearch) {
     CriteriaBuilder cb = em.getCriteriaBuilder();
     CriteriaQuery<Order> cq = cb.createQuery(Order.class);
     Root<Order> o = cq.from(Order.class);
     Join<Order, Member> m = o.join("member", JoinType.INNER); //회원과 조인
     List<Predicate> criteria = new ArrayList<>();
     //주문 상태 검색
-    if (orderSearchRepository.getOrderStatus() != null) {
-      Predicate status = cb.equal(o.get("status"), orderSearchRepository.getOrderStatus());
+    if (orderSearch.getOrderStatus() != null) {
+      Predicate status = cb.equal(o.get("status"), orderSearch.getOrderStatus());
       criteria.add(status);
     }
     //회원 이름 검색
-    if (StringUtils.hasText(orderSearchRepository.getMemberName())) {
+    if (StringUtils.hasText(orderSearch.getMemberName())) {
       Predicate name = cb.like(m.<String>get("name"), "%" +
-              orderSearchRepository.getMemberName() + "%");
+              orderSearch.getMemberName() + "%");
       criteria.add(name);
     }
     cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
